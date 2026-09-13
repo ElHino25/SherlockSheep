08975b6b55f00c6721898c8587456ce77663ee77
ElHino25 <eldalkamyasir25@gmail.com>
2026-09-13 20:45:34 +0200

    ziegfriedanimation fertig

--- Assets/Scripts/ZiegenFried.cs
+++ Assets/Scripts/ZiegenFried.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class ZiegenFried : MonoBehaviour
 {
-
-    
+  private Animator animator;
+  
+  private void Awake()
+  {
+     animator = GetComponent<Animator>();
+      
+  }
+  
+  private void OnTriggerEnter(Collider other)
+  { 
+      animator.SetBool("isDying" , true);
+  }
+      
 }
