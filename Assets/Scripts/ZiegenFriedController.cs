0ead2c0f3e3d3ba8506c76566f1c355ecaf6a4a8
ElHino25 <eldalkamyasir25@gmail.com>
2026-09-14 00:40:07 +0200

    KHALAASSS

--- Assets/Scripts/ZiegenFriedController.cs
+++ Assets/Scripts/ZiegenFriedController.cs
@@ -2,40 +2,83 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ZiegenFried : MonoBehaviour
+public class ZiegenFried : MonoBehaviour, IInteractable
 {
     [SerializeField] private string collectionText;
     [SerializeField] private string postDeathHoverText;
     [SerializeField] private string preDeathHoverText;
     [SerializeField] private string dyingHoverText;
+    [SerializeField] private string postDeathCollectionText;
+    [SerializeField] private string preDeathCollectionText;
+    [SerializeField] private string dyingCollectionText;
     
     private Animator animator;
-    private string zustand = "alive";
+    private string zustand;
     
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
+        zustand = "alive";
     }
 
     private void OnTriggerEnter(Collider other)
     {
         animator.SetBool("isDying", true);
+        if (zustand == "alive")
+        {
+           zustand = "dying"; 
+        }
+        
+    }
+    
+    private void DeathAnimationFinished()
+    {
+      zustand = "dead";
     }
 
-    private string PreDeathHoverText()
+    public string GetHoverText()
     {
-        return preDeathHoverText; 
+        if (zustand == "alive")
+        {
+            return preDeathHoverText;
+        }
+        else if  (zustand == "dying")
+        {
+            return dyingHoverText;
+        }
+        else if (zustand == "dead")
+        {
+            return postDeathHoverText;
+        }
+        else 
+        {
+            return collectionText; 
+        }
+        
     }
-    
-    private string GetDeathHoverText()
+
+    public string GetCollectionText()
     {
-        return dyingHoverText;
+        if (zustand == "alive")
+        {
+            return preDeathCollectionText;
+        }
+        else if  (zustand == "dying")
+        {
+            return dyingCollectionText;
+        }
+        else if  (zustand == "dead")
+        {
+            return postDeathCollectionText;
+        }
+        else 
+        {
+            return collectionText;
+        }
     }
-    private string GetPostDeathHoverText()
+
+    public void Collect()
     {
-        return postDeathHoverText;
+       
     }
-    
-    
 }
